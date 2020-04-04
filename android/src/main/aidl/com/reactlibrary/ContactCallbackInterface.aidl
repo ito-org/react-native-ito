1db4f5a09b2c0d6f0581311978223836104d4b8a
Christian Romberg <distjubo@gmail.com>
2020-04-05 01:08:11 +0200

    use bluetooth library from android app

--- android/src/main/aidl/com/reactlibrary/ContactCallbackInterface.aidl
+++
@@ -1,8 +0,0 @@
-// ContactCallbackInterface.aidl
-package com.reactlibrary;
-
-// Declare any non-default types here with import statements
-
-interface ContactCallbackInterface {
-    void onContactFinished(in byte[] broadcastData,  float minDistance, long startTime, long duration);
-}
--- android/src/main/java/com/reactlibrary/ItoBluetoothModule.java
+++ android/src/main/java/com/reactlibrary/ItoBluetoothModule.java
@@ -1,37 +1,40 @@
 package com.reactlibrary;
 
-import android.bluetooth.BluetoothAdapter;
-import android.content.BroadcastReceiver;
+import android.Manifest;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.ServiceConnection;
-import android.location.LocationManager;
+import android.content.pm.PackageManager;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableArray;
-import com.reactlibrary.service.TracingService;
+
+import org.itoapp.DistanceCallback;
+import org.itoapp.TracingServiceInterface;
+import org.itoapp.strict.service.TracingService;
 
 public class ItoBluetoothModule extends ReactContextBaseJavaModule {
 
     private static final String LOG_TAG = "ItoBluetoothModule";
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 4713;
     private final ReactApplicationContext reactContext;
     private TracingServiceInterface tracingServiceInterface;
-    private Callback jsContactCallback;
+    private Callback jsDistanceCallback;
 
     private ServiceConnection serviceConnection = new ServiceConnection() {
         public void onServiceConnected(ComponentName className,
                                        IBinder service) {
             tracingServiceInterface = TracingServiceInterface.Stub.asInterface(service);
             try {
-                tracingServiceInterface.setContactCallback(nativeContactCallback);
+                tracingServiceInterface.setDistanceCallback(nativeContactCallback);
             } catch (RemoteException e) {
                 Log.e(LOG_TAG, "looks like the service already crashed!", e);
             }
@@ -42,29 +45,19 @@ public class ItoBluetoothModule extends ReactContextBaseJavaModule {
         }
     };
 
-    private ContactCallbackInterface.Stub nativeContactCallback = new ContactCallbackInterface.Stub() {
+    private DistanceCallback.Stub nativeContactCallback = new DistanceCallback.Stub() {
         @Override
-        public void onContactFinished(byte[] broadcastData, float minDistance, long startTime, long duration) {
-            jsContactCallback.invoke(broadcastData, minDistance, startTime, duration);
+        public void onDistanceMeasurements(float[] distances) {
+            jsDistanceCallback.invoke(distances);
         }
     };
 
     public ItoBluetoothModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
-
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
-        filter.addAction(LocationManager.MODE_CHANGED_ACTION);
-        reactContext.registerReceiver(new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                if (TracingService.canServiceRun(context))
-                    bindService();
-                else
-                    tracingServiceInterface = null;
-            }
-        }, filter);
+        Intent intent = new Intent(reactContext, TracingService.class);
+        reactContext.startService(intent);
+        bindService();
     }
 
     private void bindService() {
@@ -78,23 +71,7 @@ public class ItoBluetoothModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void setBLEAdvertisementCallback(Callback advertisementCallback) {
-        this.jsContactCallback = advertisementCallback;
-    }
-
-    @ReactMethod
-    public void setAdvertisementData(ReadableArray advertisementData) {
-        byte[] rawBytes = new byte[advertisementData.size()];
-        for(int i = 0; i < rawBytes.length; i++) {
-            rawBytes[i] = (byte)advertisementData.getInt(i);
-        }
-
-        if(tracingServiceInterface != null) {
-            try {
-                tracingServiceInterface.setAdvertisementData(rawBytes);
-            } catch (RemoteException e) {
-                Log.e(LOG_TAG, "Could not set broadcast data!", e);
-            }
-        }
+    public void setDistanceCallback(Callback advertisementCallback) {
+        this.jsDistanceCallback = advertisementCallback;
     }
 }
