09e945e5e1af2fe430951fafe0bc754de7cd2802
Dieder Timmers <dieder@finner.cool>
2020-04-19 17:12:51 +0200

    initial stubs ios

--- ios/ItoBluetooth.m
+++ ios/ItoBluetooth.m
@@ -1,14 +1,34 @@
-#import "ItoBluetooth.h"
+#import <React/RCTBridgeModule.h>
+#import <ItoBluetooth-Swift.h>
 
 
-@implementation ItoBluetooth
+#import "ItoBluetooth-Bridging-Header.h"
+@interface RCT_EXTERN_MODULE(ItoBluetooth, NSObject)
 
-RCT_EXPORT_MODULE()
+ RCT_EXPORT_METHOD(callback:(RCTResponseSenderBlock)onDistanceMeasurements)
+ {
+     onDistanceMeasurements([NSNumber new]);
+ }
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
-{
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+RCT_EXPORT_METHOD(publishBeaconUUIDs:(NSNumber *) from:(NSNumber*) to: callback:(RCTResponseSenderBlock)onSuccess){
+    
 }
 
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(isPossiblyInfected){
+    SwItoBluetooth *sbt = [SwItoBluetooth new] ;
+    NSNumber *retval = sbt.isPossiblyInfected;
+    return retval;
+}
+
+RCT_EXPORT_METHOD(restartTracingService){
+    SwItoBluetooth *sbt = [SwItoBluetooth new] ;
+    sbt.restartTracingService;
+}
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(getLatestFetchTime){
+    SwItoBluetooth *sbt = [SwItoBluetooth new] ;
+    NSNumber *retval = sbt.getLatestFetchTime;
+    return retval;
+}
 @end
+
---
+++ ios/ItoBluetooth.swift
@@ -0,0 +1,18 @@
+//
+//  ItoBluetooth.swift
+//  ItoBluetooth
+//
+//  Created by Dieder Timmers on 18/04/2020.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
+
+@objc(ItoBluetooth)
+public class SwItoBluetooth: NSObject {
+    @objc
+    public func isPossiblyInfected() -> Bool{
+        return true;
+    }
+    
+}
