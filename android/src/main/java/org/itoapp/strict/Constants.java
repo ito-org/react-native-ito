1c4974f4688cdff3c9244cb78a4e3ed127769630
KiWo <kiwo@kiwo.net>
2020-05-06 23:38:54 +0200

    adj. constants to uber ratchet (15min tcn / 1d new rak)

--- android/src/main/java/org/itoapp/strict/Constants.java
+++ android/src/main/java/org/itoapp/strict/Constants.java
@@ -5,11 +5,12 @@ public class Constants {
     public static final int UUID_LENGTH = 16;
     public static final int HASH_LENGTH = 16;
     public static final int BROADCAST_LENGTH = HASH_LENGTH ;
-    public static final int UUID_VALID_INTERVAL = 1000 * 60 * 30; //ms * sec * 30 min
+    public static final int TCN_VALID_INTERVAL = 1000 * 60 * 15; //ms * sec * 15 min
     public static final int CHECK_SERVER_INTERVAL = 1000 * 10; //ms * 10 sec
     public static final int DISTANCE_SMOOTHING_MA_LENGTH = 7;
     public static final int CACHE_FLUSH_TIME = 1000 * 30; // 30 seconds timeout
     public static final long MIN_CONTACT_DURATION = 1000 * 60 * 1; //discard all contacts less than 1 minutes
     public static final float MIN_SCANNING_DISTANCE = 20;
     public static final long MIN_EXPOSURE_DURATION = 1000 * 60 * 2; // 2 minutes
+    public static final int RATCHET_EXCHANGE_INTERVAL = (4 * 24); // 1day? if beacon gets changed every 15min
 }
